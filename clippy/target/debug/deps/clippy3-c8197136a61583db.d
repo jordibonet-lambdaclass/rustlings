1691af792e7c3a12ccfacd4e7148cf7180d4c6ad
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-11-21 09:01:01 +0100

    Refactor

---
+++ clippy/target/debug/deps/clippy3-c8197136a61583db.d
@@ -0,0 +1,9 @@
+/Users/jordi/Documents/lambdaclass-hacking-learning-path/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-c8197136a61583db.rmeta: clippy3.rs Cargo.toml
+
+/Users/jordi/Documents/lambdaclass-hacking-learning-path/rust/rustlings/exercises/clippy/target/debug/deps/clippy3-c8197136a61583db.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
